1a013cc791b17aefa94f6d239c92e925fc43d0b3
Madhu <Madhu@Sundarams-MBP.home>
2015-07-15 18:12:03 -0400

    Grid of sample images added for Popular Movies app.

---
+++ app/src/main/java/com/example/android/popularmovies/ImageAdapter.java
@@ -0,0 +1,79 @@
+package com.example.android.popularmovies;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by Madhu on 7/15/15.
+ */
+public class ImageAdapter extends BaseAdapter {
+
+    private Context imageContext;
+
+    public ImageAdapter(Context imageContext) {
+        this.imageContext = imageContext;
+    }
+
+    public int getCount() {
+        return sampleImages.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(imageContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(185, 185));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            //imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(sampleImages[position]);
+        return imageView;
+    }
+
+    // references to our images
+    private Integer[] sampleImages = {
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_8, R.drawable.sample_9,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_8, R.drawable.sample_9,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_8, R.drawable.sample_9,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7
+    };
+}
